ecf76c4e471422175ee5f82cc52ea8a8b6ba4a97
Pratyay Bishnupuri <pbishnupuri@wustl.edu>
2020-10-06 18:05:41 -0500

    added movement

--- semester_project/Assets/Scripts/GoatController.cs
+++ semester_project/Assets/Scripts/GoatController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class GoatController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

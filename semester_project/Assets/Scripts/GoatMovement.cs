b7fcf4b33bcaa7aa8a53ce9e2ac7455dbe63fb38
Pratyay Bishnupuri <pbishnupuri@wustl.edu>
2020-10-15 22:59:09 -0500

    player falls thru ground help me

---
+++ semester_project/Assets/Scripts/GoatMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoatMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    // Start is called before the first frame update
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    Rigidbody2D _rb;
+
+    void Start() {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if(Input.GetButtonDown("Jump")){
+            jump = true;
+        }
+    }
+
+    void FixedUpdate() {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}

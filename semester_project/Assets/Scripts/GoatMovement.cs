52aacb2fa529c09c7a02f7ff26155c1c5dabe043
Pratyay Bishnupuri <pbishnupuri@wustl.edu>
2020-10-16 00:01:15 -0500

    goatmovement

--- semester_project/Assets/Scripts/GoatMovement.cs
+++ semester_project/Assets/Scripts/GoatMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class GoatMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+    float move = 0f;
+    public float runSpeed = 40f;
     void Start()
     {
         
@@ -13,22 +16,52 @@ public class GoatMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        move = Input.GetAxisRaw("Horizontal") * runSpeed;
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position += new Vector3(0, 0.2f, 0);
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.position += new Vector3(0, -0.2f, 0);
+            transform.position += new Vector3(0, 0.02f, 0);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += new Vector3(-0.2f, 0, 0);
+            transform.position += new Vector3(-0.02f, 0, 0);
+            if (move > 0 && !m_FacingRight)
+			{
+				// ... flip the player.
+				Flip();
+			}
+			// Otherwise if the input is moving the player left and the player is facing right...
+			else if (move < 0 && m_FacingRight)
+			{
+				// ... flip the player.
+				Flip();
+			}
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += new Vector3(0.2f, 0, 0);
+            transform.position += new Vector3(0.02f, 0, 0);
+            if (move > 0 && !m_FacingRight)
+			{
+				// ... flip the player.
+				Flip();
+			}
+			// Otherwise if the input is moving the player left and the player is facing right...
+			else if (move < 0 && m_FacingRight)
+			{
+				// ... flip the player.
+				Flip();
+			}
         }
         
     }
+
+    private void Flip()
+	{
+		// Switch the way the player is labelled as facing.
+		m_FacingRight = !m_FacingRight;
+
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
 }

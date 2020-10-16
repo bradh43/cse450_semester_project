68ec1d57e0d7f011c5eaefe002710cc186b66ea6
bradh43 <brad.hodkinson2@gmail.com>
2020-10-15 22:56:26 -0500

    merged into dev

---
+++ semester_project/Assets/Scripts/LevelBounds.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelBounds : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other) {
+        // Reload scene only when colliding with player
+        // TODO change to GoatController
+        if(other.gameObject.GetComponent<Squirell>()){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
---
+++ semester_project/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Obstacle : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other) {
+        // Reload scene only when colliding with player
+        // TODO change to GoatController
+        if(other.gameObject.GetComponent<Squirell>()){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
